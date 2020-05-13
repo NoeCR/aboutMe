0a92739cfb550318555bdd99cce303b3ca66bc72
NoeCR <noecr.dev@gmail.com>
2020-05-13 21:44:59 +0200

    Start with template for show information
    Signed-off-by: NoeCR <noecr.dev@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { faPenAlt, faCode, faAddressCard } from '@fortawesome/free-solid-svg-icons';
+import *  as  data from '../assets/datos.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'aboutMe';
+  icons = {
+    pencil: faPenAlt,
+    code: faCode,
+    card: faAddressCard
+  };
+  latest: any = (data as any).default;
+  
+  ngOnInit() { }
 }
